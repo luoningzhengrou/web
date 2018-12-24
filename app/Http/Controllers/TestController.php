d2ca92b587bf68bbda22000f41b8e777a35132f9
Frank <luoningzhengrou@gmail.com>
2018-12-24 11:04:02 +0800

    Initial commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Frank.F
+ * Date: 2018/12/21
+ * Time: 11:33
+ */
+
+namespace App\Http\Controllers;
+
+
+class TestController extends Controller
+{
+    public function index(){
+//        $str = "Im1ldGhvZGRlc2MiOiLku44wLTnkuK3ku7vmhI/pgInmi6ky5Liq5Lul5LiK5Y+356CB44CCIiwNCiJtZXRob2RoZWxwIjoi5LuOMC055Lit6YCJ5oupMuS4quWPt+egge+8jOavj+azqOeUsTLkuKrkuI3lkIznmoTlj7fnoIHnu4TmiJDvvIzlvIDlpZblj7fnoIHnmoTljYPkvY3jgIHnmb7kvY3jgIHljYHkvY3kuK3lkIzml7bljIXlkKvmiYDpgInnmoQy5Liq5Y+356CB77yM5Y2z5Li65Lit5aWW44CCIiwNCiJtZXRob2RleGFtcGxlIjoi5oqV5rOo5pa55qGI77yaMSwy77yb5byA5aWW5Y+356CB5Lit5LiJ5L2N77ya6Iez5bCR5Ye6546wMeWSjDLlkIQx5Liq77yM5Y2z5Lit5Lit5LiJ5LqM56CB5LiN5a6a5L2N5LiA562J5aWW44CCIiwNCiJzZWxlY3RhcmVhIjp7DQogICAgICAgInR5cGUiICAgOiAiZGlnaXRhbCIsDQogICAgICAgImxheW91dCIgOiBbDQogICAgIHsidGl0bGUiOiLkuI3lrprog4YiLCAibm8iOiIwfDF8MnwzfDR8NXw2fDd8OHw5IiwgInBsYWNlIjowLCAiY29scyI6MX0NCiAgICBdLA0KICAgICAgICJub0JpZ0luZGV4IiA6IDUsDQogICAgICAgImlzQnV0dG9uIiAgIDogdHJ1ZQ0KICAgICAgfSwNCiJzaG93X3N0ciIgOiAiWCIsDQoiY29kZV9zcCIgOiAiLCIs";
+//        print_r(base64_decode($str));exit;
+        echo 'Hello World!';
+    }
+}
