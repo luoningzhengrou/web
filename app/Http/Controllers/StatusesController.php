dfeed28f7452151f28685513b01176094ec38e69
Frank <luoningzhengrou@gmail.com>
2019-01-23 18:30:46 +0800

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Statusus;
+use App\Models\Status;
 use Auth;
 
 class StatusesController extends Controller
@@ -20,6 +20,14 @@ class StatusesController extends Controller
         ]);
         Auth::user()->statuses()->create(['content' => $request['content']]);
         session()->flash('success', '发布成功！');
+        return redirect()->back();
+    }
+
+    public function destroy(Status $status)
+    {
+        $this->authorize('destroy', $status);
+        $status->delete();
+        session()->flash('success','微博已删除！');
         return redirect()->back();
     }
 }
