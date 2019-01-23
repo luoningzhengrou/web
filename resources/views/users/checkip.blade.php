591277a2fa6e0ff70d695d2afd37584bfea4218c
Frank <luoningzhengrou@gmail.com>
2019-01-23 11:24:49 +0800

    update

--- resources/views/users/checkip.blade.php
+++ resources/views/users/checkip.blade.php
@@ -10,15 +10,21 @@
                         <tr>
                             <td>您的IP：{{ $data['ip'] }}</td>
                         </tr>
+                        @if($data['country'] !== 'XX')
                         <tr>
                             <td>国家: {{ $data['country'] }}</td>
                         </tr>
+                        @endif
+                        @if($data['region'] !== 'XX')
                         <tr>
-                            <td>地区: {{ $data['region'] }} {{ $data['city'] }} {{ $data['county'] }}</td>
+                            <td>地区： {{ $data['region'] }} @if($data['city'] !== 'XX') {{ $data['city'] }} @endif @if($data['county'] !== 'XX'){{ $data['county'] }} @endif</td>
                         </tr>
+                        @endif
+                        @if($data['isp'] !== 'XX')
                         <tr>
-                            <td>运营商: {{ $data['isp'] }}</td>
+                            <td>运营商： {{ $data['isp'] }}</td>
                         </tr>
+                        @endif
                     </table>
                 </div>
             </div>
