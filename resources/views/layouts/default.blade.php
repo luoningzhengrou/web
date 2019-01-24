192a92105169dfc6f0661f459802e60c2eeca6d5
Frank <luoningzhengrou@gmail.com>
2019-01-24 17:56:58 +0800

    add icon

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>@yield('title','Weibo App') - Laravel 入门教程</title>
+        <title>@yield('title','微博')</title>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="shortcut icon" href="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png"/>
     </head>
     <body>
         @include('layouts._header')
